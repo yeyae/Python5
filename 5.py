48b03828ee36763b236cc3dcc5e6f0a234da1022
yeyae <whyjinny@naver.com>
2022-06-22 09:48:35 +0900

    python

---
+++ 5.py
@@ -0,0 +1,28 @@
+#문자열 연산하기
+
+#1.문자열 더해서 연결하기
+head = "Python"
+tail =  " is fun!"
+head + tail
+
+a = "python"
+a*2
+
+print("=" * 50)
+print("My Program")
+print("=" * 50)
+
+#문자열 길이 구하기
+a = "Life is too short"
+len(a)
+
+b = "You need python"
+len(b)
+
+#문자열 인덱싱
+c = "Life is too short, You need Python"
+c[5]
+c[-1] #뒤에서부터 세어 첫 번째가 되는 문자
+c[0]
+c[-0]
+
